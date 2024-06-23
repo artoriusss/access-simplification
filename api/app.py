5e0de9ee6c45600e0acf2612a0c02c1c76e0ca0b
artoriusss <artorius.ua@gmail.com>
2024-06-23 19:44:20 +0000

    -commented legacy code

--- api/app.py
+++ api/app.py
@@ -3,14 +3,12 @@ from flask import Flask
 from routes.annotate import annotate_bp
 from routes.simplify import simplify_bp
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a strong secret key
+app.secret_key = 'your_secret_key'  
 
-# Register Blueprints
 app.register_blueprint(annotate_bp)
 app.register_blueprint(simplify_bp)
 
